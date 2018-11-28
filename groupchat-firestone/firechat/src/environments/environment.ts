7554fb48a4bad05ed8ee49b91e67c0001b1783b8
Tanner Driggers <tannerdriggers@ksu.edu>
2018-11-28 16:49:23 -0600

    fixed lint issues

--- groupchat-firestone/firechat/src/environments/environment.ts
+++ groupchat-firestone/firechat/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCg-TsNtyt1mCRaUHV3S4eWSu0tep1JkTg",
-    authDomain: "groupchat-e64d1.firebaseapp.com",
-    databaseURL: "https://groupchat-e64d1.firebaseio.com",
-    projectId: "groupchat-e64d1",
-    storageBucket: "",
-    messagingSenderId: "703050116846"
+    apiKey: 'AIzaSyCg-TsNtyt1mCRaUHV3S4eWSu0tep1JkTg',
+    authDomain: 'groupchat-e64d1.firebaseapp.com',
+    databaseURL: 'https://groupchat-e64d1.firebaseio.com',
+    projectId: 'groupchat-e64d1',
+    storageBucket: '',
+    messagingSenderId: '703050116846'
   }
 };
 
