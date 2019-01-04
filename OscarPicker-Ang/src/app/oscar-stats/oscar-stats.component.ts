6c0e87f1a1331f25b99260e589a7e6117690e22e
Tanner Driggers <tannerdriggers@ksu.edu>
2019-01-04 12:44:53 -0600

    async testing

--- OscarPicker-Ang/src/app/oscar-stats/oscar-stats.component.ts
+++ OscarPicker-Ang/src/app/oscar-stats/oscar-stats.component.ts
@@ -45,74 +45,70 @@ export class OscarStatsComponent implements OnInit {
     this.years$ = this.afs.collection<Year>('user_picks').valueChanges();
     this.years$.subscribe(details => {
       this.year = details[details.length - 1].year;
-    });
-    this.auth.user$.subscribe(user => this.user = user);
-    console.log('userId: ' + this.user.uid);
-    this.userAnswers$ = this.afs.collection<Choice>(`user_picks/${this.year}/Oscar/${this.user.uid}/categories`).valueChanges();
-    this.userAnswersSubscription = this.userAnswers$.subscribe(answers => {
-      this.userAns = answers;
-      console.log(this.userAns);
-    });
-    this.oscarCategory$ = this.afs.collection<OscarCategory>(`oscar_categories/${this.year}/categories`).valueChanges();
-    this.oscarCategorySubscription = this.oscarCategory$.subscribe(categories => {
-      this.oscarCategories = categories;
-      console.log(this.oscarCategories);
-    })
-  }
-
-  rightAnswer(category: string): boolean {
-    let userAns;
-    let correctAns;
-    // if (this.DoneLoading()) {
-      for (let ans of this.userAns) {
-        if (ans.category === category) {
-          userAns = ans.choice;
-          break;
-        }
-      }
-      for (let ans of this.oscarCategories) {
-        if (ans.category === category) {
-          correctAns = ans.winner;
-          break;
-        }
-      }
-      if (userAns != null) {
-        return userAns === correctAns;
-      }
-      return null;
-    // }
-    // return null;
-  }
-
-  async getYear(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.years$.subscribe(details => {
-        this.year = details[details.length - 1].year;
-        resolve();
+      this.auth.user$.subscribe(user => {
+        this.user = user;
+        this.userAnswers$ = this.afs.collection<Choice>(`user_picks/${this.year}/Oscar/${this.user.uid}/categories`).valueChanges();
       });
-    })
+      this.oscarCategory$ = this.afs.collection<OscarCategory>(`oscar_categories/${this.year}/categories`).valueChanges();
+      this.oscarCategorySubscription = this.oscarCategory$.subscribe(categories => {
+        this.oscarCategories = categories;
+        console.log('Oscar Categories: ')
+        console.log(this.oscarCategories);
+      });
+    });
   }
 
-  async getUser(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.auth.user$.subscribe(
-        user => {
-          this.user = user;
-          resolve();
+  async rightAnswer(category: string) {
+    let r;
+    // console.log(category);
+    await this.rightAnswerPromise(category).then(result => r = result);
+    console.log(category + ':' + r);
+    return r;
+  }
+
+  async rightAnswerPromise(category: string) {
+    return new Promise(resolve => {
+      let r: boolean = null;
+      this.userAnswersSubscription = this.userAnswers$.subscribe(answers => {
+        this.userAns = answers;
+        // console.log(this.userAns);
+        let userAns;
+        let correctAns;
+        for (let ans of this.userAns) {
+          // console.log(ans.category + ':' + category);
+          if (ans.category === category) {
+            userAns = ans.choice;
+            // console.log(ans.choice);
+            break;
+          }
         }
-      );
-    })
+        console.log(this.oscarCategories);
+        for (let ans of this.oscarCategories) {
+          console.log('Answer Categories= ' + ans.category + ':' + category);
+          if (ans.category === category) {
+            console.log('Winner= ' + ans.winner);
+            correctAns = ans.winner;
+            break;
+          }
+        }
+        if (userAns != null) {
+          r = userAns === correctAns;
+        }
+        console.log('Answers= ' + userAns + ':' + correctAns);
+        resolve(r);
+      });
+    });
   }
 
   YearChosen(year: string): void {
     this.year = year;
     this.userAnswers$ = this.afs.collection<Choice>(`user_picks/${this.year}/Oscar/${this.user.uid}/categories`).valueChanges();
     this.oscarCategory$ = this.afs.collection<OscarCategory>(`oscar_categories/${this.year}/categories`).valueChanges();
-  }
-
-  DoneLoading(): boolean {
-    // waitsFor(() => !!this.userAns, 'Failed', 50);
-    return true;
+    this.oscarCategorySubscription = this.oscarCategory$.subscribe(categories => {
+      this.oscarCategories = categories;
+      console.log('Oscar Categories: ');
+      console.log(this.oscarCategories);
+    })
   }
 
 }
