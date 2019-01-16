921b82d4ad7e2451f85524f8debd4e5b734daa75
Tanner Driggers <tannerdriggers@ksu.edu>
2019-01-16 10:34:39 -0600

    delete the unneeded projects

--- OscarPicks_Angular/OscarPicks_Auth0/JWTSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OscarPicks_Auth0
-{
-    public class JWTSettings
-    {
-        public string SecretKey { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-    }
-}
